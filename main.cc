dad6e2db8af15838498a191c15c1e767f9cdf52c
shmup73 <t.hauswald@yahoo.de>
2015-11-26 00:03:50 +0100

    Imported older netbeans project.

--- main.cc
+++ main.cc
@@ -1,55 +1,27 @@
-#include "util.h"
+#include "cpu.h"
+#include "mmu.h"
 
-namespace gbhd {
+int main(int argc, char** argv) {
+    using namespace gbhd;
 
-    template<typename t>
-    t factorial(t n) {
-        return n <= 1 ? 1 : n-- * factorial(n);
+    cpu cpu;
+    mmu mmu;
+
+    // Example binary.
+    byte binary[0xff];
+    memset(binary, 0x00, sizeof(binary));
+
+    cpu.dumpstate();
+
+    // CPU dispatch loop.
+    while (cpu.regs.pc < sizeof(binary)) {
+
+        // fetch -> decode -> execute.
+        const instruction& instr = cpu.iset.decode(binary[cpu.regs.pc++]);
+        instr.execute(cpu, mmu);
+
+        cpu.dumpstate();
     }
-
-    template<typename t>
-    vector<t> primeFactors(t n) {
-
-        t root = sqrt(n);
-        vector<t> factors;
-        factors.reserve(root);
-
-        if (n < 1) return { };
-        if (n == 1) return { 1 };
-
-        while (dividable(n, 2)) {
-            factors.push_back(2);
-            n /= 2;
-        }
-
-        for (t m = 3; m <= root; m += 2) {
-            while (dividable(n, m)) {
-                factors.push_back(m);
-                n /= m;
-            }
-        }
-
-        return factors;
-    }
-}
-
-int main(void) {
-
-    gbhd::benchmark("factorial", [ ](void) {
-        for (uint64_t n = 1; n < 100; ++n) {
-            std::cout << n << "! = " << gbhd::factorial(n) << std::endl;
-        }
-    });
-
-    gbhd::benchmark("primeFactors", [ ](void) {
-        for (uint64_t n = 1; n < 100; ++n) {
-            std::cout << n << " = ";
-            for (auto const& factor : gbhd::primeFactors(n)) {
-                std::cout << factor << " * ";
-            }
-            std::cout << std::endl;
-        }
-    });
 
     return 0;
 }
--- util.h
+++ util.h
@@ -5,17 +5,53 @@
 #ifndef GBHD_UTIL_H
 #define GBHD_UTIL_H
 
+/**
+ * Common STL includes.
+ */
+
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <functional>
 #include <cmath>
 #include <chrono>
 #include <vector>
+#include <cstring> // memset(...)
 
-#define dividable(num, denom) (!(num % denom))
+/**
+ * Macro definitions.
+ */
+
+#define RAM_SIZE    65536
+#define ISET_SIZE   512
+#define DIVIDABLE(num, denom) (!(num % denom))
 
 namespace gbhd {
     using namespace std;
 
-    chrono::high_resolution_clock::time_point currentTime(void) {
+    typedef uint8_t byte;  // 8-bit.
+    typedef uint16_t word;  // 16-bit
+
+    // Readable 8-bit hex string.
+    inline string hex(byte value) {
+        char buf[5];
+        snprintf(buf, 5, "0x%hhx", value);
+        return string(buf);
+    }
+
+    // Readable 16-bit hex string.
+    inline string hex(word value) {
+        char buf[7];
+        snprintf(buf, 7, "0x%hx", value);
+        return string(buf);
+    }
+
+    // Concatenates 2 8-bit wide operands to a 16-bit word.
+    inline word concat(byte hi, byte lo) {
+        return ((hi << 8) | lo);
+    }
+
+    inline chrono::high_resolution_clock::time_point currentTime(void) {
         return chrono::high_resolution_clock::now();
     }
 
